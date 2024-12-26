d92aca36d7b25c5933ba1ed8440628c8097317fb
taha-009 <muhammadistaha349@gmail.com>
2024-12-26 16:02:06 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,22 +20,15 @@ LANGCHAIN_ENDPOINT = os.getenv("LANGCHAIN_ENDPOINT")
 LANGCHAIN_API_KEY = os.getenv("LANGCHAIN_API_KEY")
 LANGCHAIN_PROJECT = os.getenv("LANGCHAIN_PROJECT")
 # Document loader
-#loader = WebBaseLoader(
-   # 'https://en.wikipedia.org/wiki/Elon_Musk',
-   # bs_kwargs=dict(parse_only=SoupStrainer(class_=('mw-content-ltr mw-parser-output')))
-#)
-#documents = loader.load()
-# Split documents into chunks
-#recursive = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
-#chunks = recursive.split_documents(documents)
-@st.cache_data
-def load_and_process_documents():
-    loader = WebBaseLoader('https://en.wikipedia.org/wiki/Elon_Musk', bs_kwargs=dict(parse_only=SoupStrainer(class_=('mw-content-ltr mw-parser-output'))))
-    documents = loader.load()
-    recursive = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
-    chunks = recursive.split_documents(documents)
-    return chunks
-load_and_process_documents()
+loader = WebBaseLoader(
+    'https://en.wikipedia.org/wiki/Elon_Musk',
+    bs_kwargs=dict(parse_only=SoupStrainer(class_=('mw-content-ltr mw-parser-output')))
+)
+documents = loader.load()
+#Split documents into chunks
+recursive = RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=100)
+chunks = recursive.split_documents(documents)
+
 # Initialize embedding and Qdrant
 embed = HuggingFaceEmbeddings(model_name='BAAI/bge-small-en-v1.5')
 
