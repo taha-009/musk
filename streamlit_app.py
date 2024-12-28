c4da34982f17fc103ef2ceb1d2d2a46c4c01b4d3
taha-009 <muhammadistaha349@gmail.com>
2024-12-28 23:38:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ def send_input():
 # Input field for queries
 with st.container():
     query = st.text_input("Please enter a query", key="query", on_change=send_input)
-    send_button = st.button("Send", key="send_btn", on_click=clear_input_field)  # Single send button
+    send_button = st.button("Send", key="send_btn", on_click=clear_input_field())  # Single send button
 
 # Chat logic
 if send_button or st.session_state.send_input:
