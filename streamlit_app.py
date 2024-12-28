c92f1a527aac0575935461380b3a5d36414da5e5
taha-009 <muhammadistaha349@gmail.com>
2024-12-28 23:34:55 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ with st.container():
     send_button = st.button("Send", key="send_btn", on_click=clear_input_field)  # Single send button
 
 # Chat logic
-if send_button or send_input and query:
+if send_button or st.session_state.send_input:
     with st.spinner("Processing... Please wait!"):  # Spinner starts here
         response = _chain.invoke({'question': query})  # Generate response
     # Update session state with user query and AI response
